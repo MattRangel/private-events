a826e446d9edac27f8bd4d328df4cb866d13edc2
Matt Rangel <matt333three@aol.com>
2023-11-01 18:38:28 -0500

    Create event class methods 'past' and 'upcoming'

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_and_belongs_to_many :attendees, class_name: 'User'
+
+  def self.past
+    Event.all.where("time <= ?", Time.now)
+  end
+
+  def self.upcoming
+    Event.all.where("time > ?", Time.now)
+  end
 end
