f89b88de8c19dae5a57c5880d8f3c6526434cf9b
Matt Rangel <matt333three@aol.com>
2023-11-01 18:47:18 -0500

    Refactor 'past' and 'upcoming' class methods into scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,11 +2,6 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_and_belongs_to_many :attendees, class_name: 'User'
 
-  def self.past
-    Event.all.where("time <= ?", Time.now)
-  end
-
-  def self.upcoming
-    Event.all.where("time > ?", Time.now)
-  end
+  scope :past, -> { where("time <= ?", Time.now) }
+  scope :upcoming, -> { where("time > ?", Time.now) }
 end
